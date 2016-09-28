609b068e6b815074a36a389bb8b90ab364124ec5
Rich Churcher <rich.churcher@gmail.com>
2016-09-28 21:06:55 +1300

    Scaffold

---
+++ PortfolioTests/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Portfolio.Controllers;
+using Xunit;
+
+namespace PortfolioTests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
