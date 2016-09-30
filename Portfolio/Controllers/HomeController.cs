0189561d1e1ef70248cbbb8213d5b0018843fe97
Rich Churcher <rich.churcher@gmail.com>
2016-09-30 18:05:28 +1300

    Tidy up

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -1,48 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using Portfolio.Models;
+﻿using System.Web.Mvc;
 
 namespace Portfolio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ApplicationDbContext _db;
-
-        public HomeController()
-        {
-            _db = new ApplicationDbContext();
-        }
-
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact details";
 
             return View();
-        }
-
-        public ActionResult History()
-        {
-            var jobs = _db.Jobs;
-            var vm = new HistoryViewModel
-            {
-                Title = "Work History",
-                Jobs = jobs.ToList()
-            };
-            return View(vm);
         }
     }
 }
