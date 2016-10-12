7a8a54e922c8d1ecd01978ec60398397a744f267
Rich Churcher <rich.churcher@gmail.com>
2016-10-13 10:09:34 +1300

    Add employers

--- Portfolio/Views/EmployersController.cs
+++ Portfolio/Views/EmployersController.cs
@@ -1,117 +1,117 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
+using System.Web;
 using System.Web.Mvc;
 using Portfolio.Models;
 
-namespace Portfolio.Controllers
+namespace Portfolio.Views
 {
-    public class JobsController : Controller
+    public class EmployersController : Controller
     {
         private readonly ApplicationDbContext _db = new ApplicationDbContext();
 
-        // GET: Jobs
-        [AllowAnonymous]
+        // GET: Employers
         public async Task<ActionResult> Index()
         {
-            return View(await _db.Jobs.ToListAsync());
+            return View(await _db.Employers.ToListAsync());
         }
 
-        // GET: Jobs/Details/5
-        [AllowAnonymous]
+        // GET: Employers/Details/5
         public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Job job = await _db.Jobs.FindAsync(id);
-            if (job == null)
+            Employer employer = await _db.Employers.FindAsync(id);
+            if (employer == null)
             {
                 return HttpNotFound();
             }
-            return View(job);
+            return View(employer);
         }
 
-        // GET: Jobs/Create
+        // GET: Employers/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Jobs/Create
+        // POST: Employers/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "Id,Start,Finish,Title,Description,Created,Updated")] Job job)
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Description")] Employer employer)
         {
-            job.Updated = DateTime.Now;
             if (ModelState.IsValid)
             {
-                _db.Jobs.Add(job);
+                _db.Employers.Add(employer);
                 await _db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
-            return View(job);
+            return View(employer);
         }
 
-        // GET: Jobs/Edit/5
+        // GET: Employers/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Job job = await _db.Jobs.FindAsync(id);
-            if (job == null)
+            Employer employer = await _db.Employers.FindAsync(id);
+            if (employer == null)
             {
                 return HttpNotFound();
             }
-            return View(job);
+            return View(employer);
         }
 
-        // POST: Jobs/Edit/5
+        // POST: Employers/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,Start,Finish,Title,Description")] Job job)
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Description")] Employer employer)
         {
-            job.Updated = DateTime.Now;
             if (ModelState.IsValid)
             {
-                _db.Entry(job).State = EntityState.Modified;
+                _db.Entry(employer).State = EntityState.Modified;
                 await _db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            return View(job);
+            return View(employer);
         }
 
-        // GET: Jobs/Delete/5
+        // GET: Employers/Delete/5
         public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Job job = await _db.Jobs.FindAsync(id);
-            if (job == null)
+            Employer employer = await _db.Employers.FindAsync(id);
+            if (employer == null)
             {
                 return HttpNotFound();
             }
-            return View(job);
+            return View(employer);
         }
 
-        // POST: Jobs/Delete/5
+        // POST: Employers/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            Job job = await _db.Jobs.FindAsync(id);
-            _db.Jobs.Remove(job);
+            Employer employer = await _db.Employers.FindAsync(id);
+            _db.Employers.Remove(employer);
             await _db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
